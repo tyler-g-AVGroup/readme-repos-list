fc859e0d9d236b50f55cc25b65eee1bb81974f93
DenverCoder1 <jonah@freshidea.com>
2021-02-08 22:58:12 +0200

    🅰 Sort alphabetically by name

--- src/index.ts
+++ src/index.ts
@@ -52,7 +52,7 @@ export const run = async () => {
     "\n<!-- This list is auto-generated using koj-co/readme-repos-list -->\n<!-- Do not edit this list manually, your changes will be overwritten -->\n";
   repos.data.items
     .filter((repo) => repo.full_name !== `${owner}/${repo}`)
-    .sort((a,b) => a.localeCompare(b))
+    .sort((a,b) => (a.name).localeCompare(b.name))
     .filter((item, index, items) =>
       getInput("one-per-owner")
         ? items.map((i) => i.owner.login).indexOf(item.owner.login) === index
